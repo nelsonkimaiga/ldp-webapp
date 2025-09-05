1b05744835b45ff6d237de39efe1d3e1222e2228
Nelson Kimaiga <nkimaiga@intellisoftkenya.com>
2025-09-06 01:05:10 +0300

    Enable CORS on SecurityConfig

--- backend/src/main/java/com/mainappldp/mainapp/config/SecurityConfig.java
+++ backend/src/main/java/com/mainappldp/mainapp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mainappldp.mainapp.config;
 import com.mainappldp.mainapp.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +27,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf(AbstractHttpConfigurer::disable)
-				.cors(cors -> cors.disable())
+				.cors(Customizer.withDefaults())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
 						.requestMatchers("/api/v1/refresh").permitAll()
